6a88af1facc4651450b4708a28cab18b1afe9d98
aroraishaan <contact.ishaanarora@gmail.com>
2020-05-01 22:13:33 -0400

    test again

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -4,5 +4,6 @@ import json
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     print("hello world")
+    print("hello world 2")
     response = ec2.describe_availability_zones()
     return {"statusCode": 200, "body": json.dumps(response)}
