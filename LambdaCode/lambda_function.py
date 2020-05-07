0de6fb79ee6ff6ec9dd03be1ee04649a828e8a8a
aroraishaan <contact.ishaanarora@gmail.com>
2020-05-07 17:12:32 -0400

    random change

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -5,5 +5,6 @@ ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     print("hello world")
     print("hello world 2")
+    print("adi yarne")
     response = ec2.describe_availability_zones()
     return {"statusCode": 200, "body": json.dumps(response)}
