1ddfd9e60959555196b27d3395810d0465557803
yarnehermann <yarne.hermann@ugent.be>
2020-05-08 15:11:18 -0400

    Add lambda functions

---
+++ LambdaCode/LF1/lambda_function.py
@@ -0,0 +1,67 @@
+import json
+import boto3
+import re
+import requests
+from requests_aws4auth import AWS4Auth
+
+import datetime
+
+region = 'us-east-1'  # e.g. us-west-1
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+host = 'https://vpc-photos-test-kxphmar6eg4ezxngicoz5kibqm.us-east-1.es.amazonaws.com'  # the Amazon ES domain, including https://
+index = 'photos-test'
+type = 'lambda-type'
+url = host + '/' + index + '/' + type
+
+headers = {"Content-Type": "application/json"}
+
+
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+def detect_labels(photo, bucket):
+    client = boto3.client('rekognition')
+
+    response = client.detect_labels(Image={'S3Object': {'Bucket': bucket, 'Name': photo}},
+                                    MaxLabels=12, MinConfidence=0.75)
+
+    # print('Detected labels for ' + photo)
+    labels = []
+    for label in response['Labels']:
+        labels.append(label['Name'])
+        # print ("Label: " + label['Name'])
+        # print ("Confidence: " + str(label['Confidence']))
+    return labels
+
+
+def lambda_handler(event, context):
+    # print("EVENT:", event)
+    # print("CONTEXT:", context)
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    img_key = event['Records'][0]['s3']['object']['key']
+    # print(bucket)
+    # print(img_key)
+
+    # 1) Call Rekognition to get labels
+    labels = detect_labels(img_key, bucket)
+
+    # 2) Store JSON obbject in ElasticSearch
+    timestamp = datetime.datetime.now()
+    timestamp_str = timestamp.strftime("%Y-%m-%dT%H:%M:%S")
+
+    document = {
+        "objectKey": img_key,
+        "bucket": bucket,
+        "createdTimestamp": timestamp_str,
+        "labels": labels
+    }
+
+    r = requests.post(url, auth=awsauth, json=document, headers=headers)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
---
+++ LambdaCode/LF2/lambda_function.py
@@ -0,0 +1,104 @@
+import json
+import boto3
+from elasticsearch import Elasticsearch
+import certifi
+
+
+def disambiguate_query(query, userId):
+    # return object is dict with slotvalues
+    lex = boto3.client('lex-runtime')
+    response = lex.post_text(
+        botName='search_photos_bot',
+        botAlias='search_photos_bot',
+        userId=userId,
+        inputText=query
+    )
+    print(response)
+    slots = response['slots']
+    res = []
+    if slots['key_a'] is not None:
+        key_a = slots['key_a']
+        if key_a[-1] == 's':
+            key_a = key_a[:-1]
+        res.append(key_a)
+    if slots['key_b'] is not None:
+        key_b = slots['key_b']
+        if key_b[-1] == 's':
+            key_b = key_b[:-1]
+        res.append(key_b)
+
+    print(res)
+    return res
+
+
+def search_index(keywords):
+    # Use elasticsearch library
+    # es_host = es_domain_endpoint('photos-test')
+
+    es = Elasticsearch(['https://vpc-photos-test-kxphmar6eg4ezxngicoz5kibqm.us-east-1.es.amazonaws.com'],
+                       ##[{'host': es_host, 'port': 443}  ], #
+                       use_ssl=True,
+                       ca_certs=certifi.where()
+                       )
+
+    # each entry in results should be of the form {url: string, labels: array (of strings)}
+    if len(keywords) == 1:
+        query = {
+            "query": {
+                "bool": {
+                    "must": {
+                        "match_all": {}
+                    },
+                    "filter": {
+                        "term": {"labels": keywords[0]}
+                    }
+                }
+            }
+        }
+    elif len(keywords) == 2:
+        query = {
+            "query": {
+                "bool": {
+                    "must": {
+                        "match_all": {}
+                    },
+                    "filter": {
+                        "term": {"labels": keywords[0]},
+                        "term": {"labels": keywords[1]}
+                    }
+                }
+            }
+        }
+
+    es_res = es.search(index='photos-test', body=query)
+    res = []
+    for hit in es_res['hits']['hits']:
+        obj = hit['_source']
+        res.append({'url': ('https://' + obj['bucket'] + '.s3.amazonaws.com/' + obj['objectKey']), 'labels': obj['labels']})
+
+    print(res)
+    return res
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    print(event)
+    query = event['query']
+    userId = "dummy"  # does not matter since lex does not need to  keep state
+
+    # 1) Pass query to Lex to get keywords
+    keywords = disambiguate_query(query, userId)
+
+    if not keywords:
+        return {
+            'statusCode': 200,
+            'results': []
+        }
+
+    # 2) search elasticsearch for photos on the keywords
+    results = search_index(keywords)
+
+    return {
+        'statusCode': 200,
+        'results': results
+    }
